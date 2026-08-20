5c1266864e34a11bcafb1b95ae7bfdd80e64f7ba
klaraplivova <126472015+klaraplivova@users.noreply.github.com>
2026-08-20 11:24:32 +0200

    display

--- arduino_hydroponics/arduino_hydroponics.ino
+++ arduino_hydroponics/arduino_hydroponics.ino
@@ -15,7 +15,7 @@ DIYables_LCD_I2C lcd(0x27, 16, 2);
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature tempSensors(&oneWire);
 
-// reference voltage and sample buffer lenght
+// reference voltage and sample buffer length
 #define VREF 5.0       
 #define SCOUNT 30          
 int analogBuffer[SCOUNT];
@@ -23,6 +23,7 @@ int analogBufferIndex = 0;
 
 void setup() {
   Serial.begin(9600);
+  
   // initialize display
   lcd.init();
   lcd.backlight();
@@ -33,6 +34,11 @@ void setup() {
   
   pinMode(TDS_PIN, INPUT);
 }
+
+void loop() {
+  static unsigned long sampleTime = millis();
+  static unsigned long displayTime = millis();
+
   // sample every 40 ms
   if (millis() - sampleTime > 40) {
     sampleTime = millis();
@@ -43,10 +49,6 @@ void setup() {
   // update display every second
   if (millis() - displayTime > 1000) {
     displayTime = millis();
-
-void loop() {
-  static unsigned long sampleTime = millis();
-  static unsigned long displayTime = millis();
     
     // read temp
     tempSensors.requestTemperatures();
@@ -62,15 +64,27 @@ void loop() {
     // convert to voltage
     float averageVoltage = averageAnalog * (float)VREF / 1024.0;
 
-
-     // temp compensation factor
+    // temp compensation factor
     float compensationCoefficient = 1.0 + 0.02 * (currentTemp - 25.0);
     float compensationVoltage = averageVoltage / compensationCoefficient;
 
-    // convert voltage to cond value
+    // convert voltage to TDS value
     float tdsValue = (133.42 * compensationVoltage * compensationVoltage * compensationVoltage 
                      - 255.86 * compensationVoltage * compensationVoltage 
                      + 857.39 * compensationVoltage) * 0.5;
-    
-    }
+
+    // print values to LCD
+    lcd.clear();
+
+    lcd.setCursor(0, 0);
+    lcd.print("temp: ");
+    lcd.print(currentTemp, 1);
+    lcd.write(223); // Degree symbol (°)
+    lcd.print("C");
+
+    lcd.setCursor(0, 1);
+    lcd.print("cond: ");
+    lcd.print((int)tdsValue);
+    lcd.print(" ppm");
+  }
 }
